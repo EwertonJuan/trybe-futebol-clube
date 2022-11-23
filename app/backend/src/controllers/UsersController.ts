9a74476fd11325d95105196510a55e17a0f90683
Ewerton Juan <juancunha2011@hotmail.com>
2022-11-23 19:40:01 -0300

    creating UsersController.login and routes

--- app/backend/src/controllers/UsersController.ts
+++ app/backend/src/controllers/UsersController.ts
@@ -3,6 +3,7 @@ import UsersService from '../services/UsersService';
 
 export default class UsersController {
   static async login(req: Request, res: Response) {
+    console.log(req.body);
     const token = await UsersService.login(req.body);
     res.status(200).json(token);
   }
