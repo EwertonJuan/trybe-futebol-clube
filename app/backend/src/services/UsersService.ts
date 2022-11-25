e53ee4709bbbddcf3a181482af02d518f17554a1
Ewerton Juan <juancunha2011@hotmail.com>
2022-11-25 19:00:45 -0300

    creating method UsersService.validateLogin

--- app/backend/src/services/UsersService.ts
+++ app/backend/src/services/UsersService.ts
@@ -1,13 +1,21 @@
+import { Op } from 'sequelize';
 import JwtValidation from '../utils/JwtValidation';
 import UsersModel from '../database/models/UsersModel';
 import ILogin from '../interfaces/ILogin';
+import IUser from '../interfaces/IUser';
 
 export default class UsersService {
+  static validateLogin(login: ILogin, user: IUser) {
+    if (login.email !== user.email || login.password !== user.password) {
+      throw new Error('Incorrect email or password');
+    }
+  }
+
   static async login(user: ILogin): Promise<string> {
     const userExists = await UsersModel
-      .findOne({ where: { email: user.email, password: user.password } });
+      .findOne({ where: { [Op.or]: [{ email: user.email }, { password: user.password }] } });
     if (userExists) {
-      throw new Error('User already exists');
+      this.validateLogin(user, userExists);
     }
     const token = await JwtValidation.createToken(user);
     return token;
