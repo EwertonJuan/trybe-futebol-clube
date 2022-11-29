e9b85dd30e33a988146919ec2a1e732d59f19bc1
Ewerton Juan <juancunha2011@hotmail.com>
2022-11-29 17:16:52 -0300

    creating tests for GET /teams

---
+++ app/backend/src/tests/teams.test.ts
@@ -0,0 +1,38 @@
+import * as sinon from 'sinon';
+import * as chai from 'chai';
+// @ts-ignore
+import chaiHttp = require('chai-http');
+
+import App from '../app';
+import TeamsModel from '../database/models/TeamsModel';
+import JwtValidation from '../utils/JwtValidation';
+
+import { Response } from 'superagent';
+import { teams } from './mocks/teams.mock';
+
+chai.use(chaiHttp);
+
+const { app } = new App();
+
+const { expect } = chai;
+
+describe('Testing route /teams', () => {
+  let chaiHttpResponse: Response;
+
+  before(async () => {
+    sinon.stub(TeamsModel, "findAll").resolves(teams as TeamsModel[]);
+  });
+  
+  after(()=>{
+    (TeamsModel.findAll as sinon.SinonStub).restore();
+  });
+
+  it('returns all teams with GET', async () => {
+    chaiHttpResponse = await chai
+      .request(app)
+      .get('/teams');
+    
+    expect(chaiHttpResponse.status).to.be.equal(200);
+    expect(chaiHttpResponse.body).to.be.deep.equal(teams);
+  })
+})
