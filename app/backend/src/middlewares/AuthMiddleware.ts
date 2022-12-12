6e718ffe9eab4b23c5cf973065609455494fe914
Ewerton Juan <juancunha2011@hotmail.com>
2022-12-12 15:06:38 -0300

    refactoring AuthMiddleware

--- app/backend/src/middlewares/AuthMiddleware.ts
+++ app/backend/src/middlewares/AuthMiddleware.ts
@@ -2,15 +2,13 @@ import { NextFunction, Request, Response } from 'express';
 import JwtValidation from '../utils/JwtValidation';
 
 export default class AuthMiddleware {
-  static async validateToken(req: Request, res: Response, next: NextFunction) {
+  static validateToken(req: Request, res: Response, next: NextFunction) {
     const { authorization } = req.headers;
-    try {
-      const auth = JwtValidation.validateToken(authorization as string);
-      req.body.user = auth;
-      next();
-    } catch (error) {
-      console.log(error);
+    const auth = JwtValidation.validateToken(authorization as string);
+    if (auth === 'Unauthorized') {
       return res.status(401).json({ message: 'Token must be a valid token' });
     }
+    req.body.user = auth;
+    next();
   }
 }
